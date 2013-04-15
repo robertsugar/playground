460c5d8f2091f3f9fbcbe2aeae40f4dfb3219967
sugar <sugar@arwen.chu.cam.ac.uk>
2013-04-16 00:27:22 +0100

    updated with comments

--- sex.sh
+++ sex.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-tail -c+72 < $0 | gunzip
+#Self extracting gzip archives
+#Robert Sugar (robert.sugar@ebi.ac.uk) 2013
+#
+#Usage: 
+#
+#1. append .gz file:
+#	cat sex.sh xxx.gz > xxx.sex 
+#2. make it executable:
+#	chmod +x xxx.sex
+#3. decompress:
+#	./xxx.sex
+#+1: use the file as a command line parameter:
+#	mytool <(xxx.sex)
+
+tail -c+352 < $0 | gunzip
 exit 0
 -----append after this----
